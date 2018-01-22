23d8e597e4f9b7341f9c7bfe04aa85c041311ea8
DaryaAleksMak <darya.aleks.mak@gmail.com>
2018-01-22 14:58:39 +0200

    create class Hanhar

--- src/main/java/entity/Hanhar.java
+++ src/main/java/entity/Hanhar.java
@@ -1,90 +1,137 @@
-import entity.plane.builder.*;
-import entity.plane.company.Airbus;
-import entity.plane.company.McDonnel;
-import entity.plane.company.Tupolev;
-import impl.Plane;
+package entity;
 
+import entity.plane.builder.*;
+import impl.Plane;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Start {
-    List<Plane>  
-    public static void main(String[] args) {
-        Plane an_2p = new AntonovBuilder()
+
+public class Hanhar {
+    static Plane an_2;
+    static Plane an_2p;
+    static Plane tu_124;
+    static Plane tu_134;
+    static Plane boeing_747;
+    static Plane boeing_777;
+    static Plane airbus_319;
+    static Plane airbus_320;
+    static Plane airbus_321;
+    static Plane md_83;
+    static Plane md_90;
+    static {
+        an_2p = new AntonovBuilder()
                 .buildModel("AN-2P")
                 .buildCountSeats(800)
                 .buildFuelConsumption(140)
                 .buildCarrierCapacity(120)
                 .build();
 
-        Plane an_2 = new AntonovBuilder()
+        an_2 = new AntonovBuilder()
                 .buildModel("AN-2")
                 .buildCountSeats(50)
                 .buildFuelConsumption(120)
                 .buildCarrierCapacity(90)
                 .build();
 
-        Plane tu_124 = new TupolevBuilder()
+        tu_124 = new TupolevBuilder()
                 .buildModel("TU-124")
                 .buildCountSeats(100)
                 .buildFuelConsumption(2850)
                 .buildCarrierCapacity(120)
                 .build();
 
-        Plane tu_134 = new TupolevBuilder()
+        tu_134 = new TupolevBuilder()
                 .buildModel("TU-134")
                 .buildCountSeats(70)
                 .buildFuelConsumption(3000)
                 .buildCarrierCapacity(150)
                 .build();
 
-        Plane boeing_747 = new BoeingBuilder()
+        boeing_747 = new BoeingBuilder()
                 .buildModel("Boeing-747")
                 .buildCountSeats(250)
                 .buildFuelConsumption(7000)
                 .buildCarrierCapacity(170)
                 .build();
-        Plane boeing_777 = new BoeingBuilder()
+        boeing_777 = new BoeingBuilder()
                 .buildModel("Boeing-777")
                 .buildCountSeats(300)
                 .buildFuelConsumption(7500)
                 .buildCarrierCapacity(170)
                 .build();
 
-        Plane airbus_319 = new AirbusBuilder()
+        airbus_319 = new AirbusBuilder()
                 .buildModel("A-319")
                 .buildCountSeats(400)
                 .buildFuelConsumption(2350)
                 .buildCarrierCapacity(200)
                 .build();
 
-        Plane airbus_320 = new AirbusBuilder()
+        airbus_320 = new AirbusBuilder()
                 .buildModel("A-320")
                 .buildCountSeats(450)
                 .buildFuelConsumption(2500)
                 .buildCarrierCapacity(250)
                 .build();
 
-        Plane airbus_321 = new AirbusBuilder()
+        airbus_321 = new AirbusBuilder()
                 .buildModel("A-321")
                 .buildCountSeats(500)
                 .buildFuelConsumption(2885)
                 .buildCarrierCapacity(275)
                 .build();
 
-        Plane md_83 = new McDonnelBuilder()
+        md_83 = new McDonnelBuilder()
                 .buildModel("MD-83")
                 .buildCountSeats(500)
                 .buildFuelConsumption(3100)
                 .buildCarrierCapacity(280)
                 .build();
 
-        Plane md_90 = new McDonnelBuilder()
+        md_90 = new McDonnelBuilder()
                 .buildModel("MD-90")
                 .buildCountSeats(500)
                 .buildFuelConsumption(2650)
                 .buildCarrierCapacity(300)
                 .build();
+    }
+
+    static List<Plane> planes = new ArrayList<Plane>(){{
+        add(an_2);
+        add(an_2p);
+        add(tu_124);
+        add(tu_134);
+        add(boeing_747);
+        add(boeing_777);
+        add(airbus_319);
+        add(airbus_320);
+        add(airbus_321);
+        add(md_83);
+        add(md_90);
+    }};
 
 
+//    private  void listInit(){
+//        planes = new ArrayList<Plane>(){{
+//            add(an_2);
+//            add(an_2p);
+//            add(tu_124);
+//            add(tu_134);
+//            add(boeing_747);
+//            add(boeing_777);
+//            add(airbus_319);
+//            add(airbus_320);
+//            add(airbus_321);
+//            add(md_83);
+//            add(md_90);
+//        }};
+//
+//    }
+
+
+
+
+    public static List<Plane> getPlanes() {
+        return planes;
     }
 }
